0ee1cc96a1a584a489857b4ca478e5629eccd6d4
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-11-13 21:06:57 -1000

    com1

---
+++ app/imports/ui/pages/Geo.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid, Image, Header } from 'semantic-ui-react';
+
+/** A simple static component to render some text for the landing page. */
+class Geo extends React.Component {
+  render() {
+    return (
+        <div className='wallpaper-background'>
+        <Grid verticalAlign='middle' textAlign='center' container>
+
+          <Grid.Column width={4}>
+            <Image size='large' src="/images/google.html"/>
+          </Grid.Column>
+
+          <Grid.Column width={8}>
+            <Header as='h1' inverted>Attach a Geotag</Header>
+            <Header as='h3' inverted>Help us maintain our UH Manoa campus</Header>
+            <Image size='medium' src="/images/report-button.png"/>
+          </Grid.Column>
+
+        </Grid>
+        </div>
+    );
+  }
+}
+
+export default Geo;
