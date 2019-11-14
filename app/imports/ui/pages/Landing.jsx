aef5a698e00a740c0eb41d622990b39c4ea1ba36
Roman Nicolai <54918828+romannicolai@users.noreply.github.com>
2019-11-13 15:31:49 -1000

    need to bring footer more down

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
+        <div className='wallpaper-background'>
         <Grid verticalAlign='middle' textAlign='center' container>
 
           <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+            <Image size='medium' src="/images/wrench.jpg"/>
           </Grid.Column>
 
           <Grid.Column width={8}>
-            <h1>Welcome to Manoa Fixed</h1>
-            <p>Help us maintain our UH Manoa campus</p>
+            <Header as='h1' inverted>Welcome to Manoa Fixed</Header>
+            <Header as='h3' inverted>Help us maintain our UH Manoa campus</Header>
           </Grid.Column>
 
         </Grid>
+        </div>
     );
   }
 }
