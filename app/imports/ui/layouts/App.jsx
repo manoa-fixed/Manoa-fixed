007374726bca863386d3b06b81aece34b80973a7
Corben Broennimann <54660123+corbenbroennimann@users.noreply.github.com>
2019-11-19 13:31:22 -1000

    Add Report Protected, Login text white

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -31,7 +31,7 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/list" component={ListReports}/>
-              <Route path="/add" component={AddReport}/>
+              <ProtectedRoute path="/add" component={AddReport}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
               <ProtectedRoute path="/signout" component={Signout}/>
