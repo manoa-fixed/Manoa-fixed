fe8de152106513fcbe27ca7c19bd73127193b997
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-11-21 11:01:35 -1000

    About page chaned to About

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -19,7 +19,7 @@ class NavBar extends React.Component {
             [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Report</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Reports</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/map" key='map'>View Map</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/about" key='map'>About page</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/about" key='map'>About</Menu.Item>,
             ]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
