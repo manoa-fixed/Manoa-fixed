e9b793b022319f3e914b3e4c72a9e3993e09dc08
Corben Broennimann <54660123+corbenbroennimann@users.noreply.github.com>
2019-11-21 11:04:18 -1000

    Inital Changes

--- app/imports/ui/pages/About.jsx
+++ app/imports/ui/pages/About.jsx
@@ -10,13 +10,12 @@ class About extends React.Component {
         <Grid verticalAlign='middle' textAlign='Left' container>
           <Grid.Column width={15}>
             <Header as='h1' inverted>Welcome to Manoa Fixed</Header>
-            <Header as='h3' inverted>This is an online application to report problems that happends at UH.
-            Some of the common problems are: Broken lights, Broken equiptment, lost item,
+            <Header as='h3' inverted>Manoa Fixed is an application to empower you to help improve the campus!
+              This is an online application to report damages and malfunctions around UH Manoa.
+            These reports may include: faulty lights, structural damages, vandalism,
               and hazards such as broken glass on the ground.
-            Use this app to suggest the college about how it can improve! Take a photo on your phone,
-              add tags and description and click the report button to send us your report!
-              You can even add geo tags to your report.
-              Start a new report by typing in your descriptions.
+            Just take out your phone and take a photo of any damages you spot, then press the Report Button to begin!
+              Next, add tags, a description, your email, a geo tag and click the submit button to send us your report!
               The site will automatically search for similar cases.
               If you find a case that matches what you want
              to report, simply click the &quot;me too&quot; button.
