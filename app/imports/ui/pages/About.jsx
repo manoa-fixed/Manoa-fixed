427f379c05ee5c64326208296ea309e8c5025b88
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-11-23 19:49:15 -1000

    Update About.jsx

--- app/imports/ui/pages/About.jsx
+++ app/imports/ui/pages/About.jsx
@@ -9,6 +9,7 @@ class About extends React.Component {
         <div className='wallpaper-background'>
         <Grid verticalAlign='middle' textAlign='Left' container>
           <Grid.Column width={15}>
+
             <Header as='h1' inverted>Welcome to Manoa Fixed</Header>
             <Header as='h3' inverted>This is an online application to report problems that happends at UH.
             Some of the common problems are: Broken lights, Broken equiptment, lost item,
@@ -20,7 +21,14 @@ class About extends React.Component {
               The site will automatically search for similar cases.
               If you find a case that matches what you want
              to report, simply click the &quot;me too&quot; button.
-
+            <Header as='h1' inverted> Welcome! </Header>
+            <Header as='h3' inverted>Manoa Fixed is an online web-tool designed
+              to help create and organize campus maintenance related
+            reports into one single source. If you are on campus
+              and see anything that needs upkeeping (potholes, broken railings,
+            flickering lights, etc), please feel free to click on our
+              Add Report Button below to begin a report. Also we have now
+              incorporated Lost & Found reports!</Header>
               Realize anyway the university can improve? Send us your suggestion!
               You can make an account to get back notifications on your report. If you
             don&apos;t want your name attached to your report,
