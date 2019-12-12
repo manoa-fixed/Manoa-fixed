ced60865fb2f87fcde3968a39165d166b9f88c83
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-12-12 11:07:46 -1000

    80% done

--- app/imports/ui/pages/About.jsx
+++ app/imports/ui/pages/About.jsx
@@ -22,39 +22,22 @@ class About extends React.Component {
                       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen></iframe>
               <Header as='h3' inverted>
+                <Header as='h1' inverted> Welcome! </Header>
                 This is an online application to report problems that happends at UH.
                 Some of the common problems are: Broken lights, Broken equiptment, lost item,
                 and hazards such as broken glass on the ground.
-                Use this app to suggest the college about how it can improve! Take a photo on your phone,
+                Use this app to suggest the college about how it can improve!
+                Take a photo on your phone, upload it to your desktop,
                 add tags and description and click the report button to send us your report!
-                You can even add geo tags to your report.
-                Start a new report by typing in your descriptions.
-                The site will automatically search for similar cases.
-                If you find a case that matches what you want
-                to report, simply click the &quot;me too&quot; button.
-                <Header as='h1' inverted> Welcome! </Header>
                 <Header as='h3' inverted>Manoa Fixed is an online web-tool designed
                   to help create and organize campus maintenance related
                   reports into one single source. If you are on campus
                   and see anything that needs upkeeping (potholes, broken railings,
                   flickering lights, etc), please feel free to click on our
-                  Add Report Button below to begin a report. Also we have now
-                  incorporated Lost & Found reports!</Header>
+                  Add Report Button below to begin a report.</Header>
                 Realize anyway the university can improve? Send us your suggestion!
-                You can make an account to get back notifications on your report. If you
-                don&apos;t want your name attached to your report,
-                just click the &quot;keep me anonymous&quot; button.
-                You can comeback to your report to check up on it anonymously with the report number.
+                You can make an account to get back notifications on your report.
                 Start a new report with the button below.</Header>
-
-              <Header as='h1' inverted> Welcome! </Header>
-              <Header as='h3' inverted>Manoa Fixed is an online web-tool
-                designed to help create and organize campus maintenance related
-                reports into one single source. If you are on campus and see
-                anything that needs upkeeping (potholes, broken railings,
-                flickering lights, etc), please feel free to click on our
-                Add Report Button below to begin a report. Also we have now
-                incorporated Lost & Found reports!</Header>
 
 
             <Button className="reportButton" color="red" size='massive' as={NavLink}
