710746953c293c92979fbcdf1fcfec060bb4a4e3
Corben Broennimann <54660123+corbenbroennimann@users.noreply.github.com>
2019-12-17 13:52:08 -1000

    Fixed Startup issue

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,16 +1,16 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Reports } from '../../api/report/Reports.js';
 
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
 function addData(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.insert(data);
+  Reports.insert(data);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.find().count() === 0) {
+if (Reports.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
